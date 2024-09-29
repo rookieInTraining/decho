d2b51cb1c43b8931eaa107e4bc25c549995760ab
rookieInTraining <rookieInTraining@github.io>
2024-09-29 20:48:11 +0530

    Initial commits for oak server

---
+++ core/logger.ts
@@ -0,0 +1,28 @@
+import * as log from "jsr:@std/log";
+
+await log.setup({
+    //define handlers
+    handlers: {
+        console: new log.ConsoleHandler("DEBUG", {
+            formatter: (record) => `${record.datetime} ${record.levelName} ${record.msg}`
+        }),
+        file: new log.RotatingFileHandler('INFO', {
+            filename: `./server-${new Date().getTime()}.log`,
+            maxBytes: 15,
+            maxBackupCount: 5,
+            formatter: rec => JSON.stringify({region: rec.loggerName, ts: rec.datetime, level: rec.levelName, data: rec.msg})})
+    },
+    //assign handlers to loggers  
+    loggers: {
+        default: {
+            level: "DEBUG",
+            handlers: ["console"],
+        },
+        client: {
+            level: "INFO",
+            handlers: ["file"]
+        }
+    },
+});
+
+export const logger = log.getLogger();
